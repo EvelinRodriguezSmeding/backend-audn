8b8e87fe0e65a297e735b5a82bac50ab63e8f83c
EvelinRodriguezSmeding <evelin_rs17@outlook.com>
2025-05-12 20:04:54 -0300

    change index.js

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ app.use(
   })
 );
 
+// Add this middleware to enable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://frontend-audn.vercel.app/'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Add the headers you need
+  next();
+});
+
 
 app.use("/api", canciones);
 app.use("/api", buscador);
