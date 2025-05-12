662ec1ccf5995b52f764aa60c4b28ddd9239c2d4
EvelinRodriguezSmeding <evelin_rs17@outlook.com>
2025-05-12 20:18:29 -0300

    fix index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(
 
 // Add this middleware to enable CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://frontend-audn.vercel.app/'); 
+  res.setHeader('Access-Control-Allow-Origin', 'https://frontend-audn.vercel.app'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Add the headers you need
   next();
