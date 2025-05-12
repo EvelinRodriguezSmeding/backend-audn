d3f7f2589d7767dd34b16513235914170f192c89
EvelinRodriguezSmeding <evelin_rs17@outlook.com>
2025-05-12 19:51:34 -0300

    update index.js

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
+require("dotenv").config();
+
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
@@ -25,7 +27,6 @@ app.use(
   })
 );
 
-require("dotenv").config();
 
 app.use("/api", canciones);
 app.use("/api", buscador);
