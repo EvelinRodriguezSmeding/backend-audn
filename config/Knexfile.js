d6405525595ab656ba3fc05cb857003f1af54d90
EvelinRodriguezSmeding <evelin_rs17@outlook.com>
2025-05-20 18:23:07 -0300

    update config

--- config/Knexfile.js
+++ config/Knexfile.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+/* const knex = require('knex')(knexConfig); */
+const knex = require('knex')(knexConfig);
 
-const knexConfig = {
+/* const knexConfig = {
   client: 'pg',
   connection: process.env.DATABASE_URL
     ? `${process.env.DATABASE_URL}?sslmode=require`  // Para producción (Render)
@@ -10,9 +12,26 @@ const knexConfig = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
       }, // Para desarrollo local
+}; */
+
+
+const knexConfig = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    }
+  : {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    }
+
 };
 
-const knex = require('knex')(knexConfig);
+
 
 module.exports = knex;
 
