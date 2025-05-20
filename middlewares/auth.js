98a3d08e5c55b0cc484a6a4b1f51394ddd16a844
EvelinRodriguezSmeding <evelin_rs17@outlook.com>
2025-05-20 19:12:13 -0300

    change auth.js jwt

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const verifyToken = (req, res, next) => {
+/* const verifyToken = (req, res, next) => {
   const token = req.header("Authorization");
   if (!token) {
     res.status(401).json({ error: "Acceso al recurso denegado" });
@@ -15,6 +15,29 @@ const verifyToken = (req, res, next) => {
   } catch (error) {
     res.status(400).json({ error: "El token es invalido", mensaje: error });
   }
+}; */
+
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+  if (!authHeader) {
+    return res.status(401).json({ error: "Acceso al recurso denegado" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "Token no proporcionado" });
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).json({ error: "El token es invalido", mensaje: error.message });
+  }
 };
+
+
 
 module.exports = {verifyToken}
